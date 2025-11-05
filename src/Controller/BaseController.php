7c834c569c9d9f129e984a41225150de352bc961
monte7292 <alejandromonterodeltoro@gmail.com>
2025-11-05 17:08:03 +0100

    Actualizo controladores, entidades y plantillas

---
+++ src/Controller/BaseController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+
+use App\Entity\Categoria;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[IsGranted('ROLE_USER')]
+final class BaseController extends AbstractController
+{
+    #[Route('/categorias', name: 'categorias')]
+    public function mostrar_categorias(EntityManagerInterface $doctrine): Response
+    {
+        $categorias = $doctrine->getRepository(Categoria::class)->findAll();
+        
+        return $this->render('categorias/mostrar_categorias.html.twig', [
+            'categorias' => $categorias,
+        ]);
+    }
+    
+}
